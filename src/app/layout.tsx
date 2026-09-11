248dd0012de3a877d53504cf697d4886715b7a38
Miguel Alejandro Paz Albornoz <129473218+2001mapa@users.noreply.github.com>
2026-09-10 19:46:25 -0500

    🔧 Fix: Habilitar zoom nativo en dispositivos móviles

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,7 +28,8 @@ export const viewport: Viewport = {
   themeColor: "#FCFAF7",
   width: "device-width",
   initialScale: 1,
-  maximumScale: 1,
+  maximumScale: 5,
+  userScalable: true,
 };
 
 export default function RootLayout({
